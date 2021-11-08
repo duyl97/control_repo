2eb14ed10176b69caa37c0b2e51e9ac3a4627ec3
duyl97 <33739713+duyl97@users.noreply.github.com>
2021-11-08 21:54:36 +0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,5 @@
 node default {
-  file {'/root/README':
-    ensure => file,
-    content => 'This is README',
-    owner => 'root',
-  }
+}
+node 'master.puppet.vm' {
+  include role::master_server
 }
